63f7c4aa5e68b2ce62a07e28ac9fbd852120202c
thelastgoth128 <thindwacyrus86@gmail.com>
2026-01-31 16:10:22 +0200

    feat: function to generate random int for interpolation

--- headers/rtweekend.h
+++ headers/rtweekend.h
@@ -3,6 +3,8 @@
  
 #include <cmath> 
 #include <iostream> 
+#include <cstdlib>
+#include <random>
 #include <limits> 
 #include <memory> 
 
@@ -23,10 +25,21 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385; 
  
 // Utility Functions 
+
+inline double random_double() { 
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0); 
+    static std::mt19937 generator; 
+    return distribution(generator); 
+}
  
 inline double degrees_to_radians(double degrees) { 
     return degrees * pi / 180.0; 
-} 
+}
+ 
+inline double random_double(double min, double max) { 
+    // Returns a random real in [min,max). 
+    return min + (max-min)*random_double(); 
+}
  
 // Common Headers 
  
