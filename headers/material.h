f2c7114081f2c5e1b07d86e6f9234036f04ff3cc
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-09 21:24:56 +0200

    material class

---
+++ headers/material.h
@@ -0,0 +1,19 @@
+#ifndef MATERIAL_H 
+#define MATERIAL_H 
+ 
+#include "hittable.h" 
+ 
+class material { 
+  public: 
+    virtual ~material() = default; 
+ 
+    virtual bool scatter( 
+        const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered 
+    ) const { 
+        return false; 
+    } 
+}; 
+
+
+ 
+#endif
