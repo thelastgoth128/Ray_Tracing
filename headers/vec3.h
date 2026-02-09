7a98d1068c336425f52ebccbf7ddd007b93d997f
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-09 21:50:37 +0200

    function to detect if the reflection is near zero

--- headers/vec3.h
+++ headers/vec3.h
@@ -47,6 +47,12 @@ class vec3 {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    bool near_zero() const { 
+        // Return true if the vector is close to zero in all dimensions. 
+        auto s = 1e-8; 
+        return (std::fabs(e[0]) < s) && (std::fabs(e[1]) < s) && (std::fabs(e[2]) < s); 
+    }
+
 
      static vec3 random() { 
         return vec3(random_double(), random_double(), random_double()); 
@@ -91,11 +97,17 @@ inline vec3 operator/(const vec3& v, double t) {
     return (1/t) * v; 
 } 
 
+
 inline double dot(const vec3& u, const vec3& v) { 
     return u.e[0] * v.e[0] 
          + u.e[1] * v.e[1] 
          + u.e[2] * v.e[2]; 
 } 
+
+inline vec3 reflect(const vec3& v, const vec3& n) { 
+    return v - 2*dot(v,n)*n; 
+}
+
  
 inline vec3 cross(const vec3& u, const vec3& v) { 
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1], 
