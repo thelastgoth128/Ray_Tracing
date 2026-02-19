fd539d531d3a0d60cca2f2f742153eb4e925c539
thelastgoth128 <thindwacyrus86@gmail.com>
2026-02-19 18:03:49 +0200

    refract method

--- headers/vec3.h
+++ headers/vec3.h
@@ -108,6 +108,13 @@ inline vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n; 
 }
 
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) { 
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0); 
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n); 
+    vec3 r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n; 
+    return r_out_perp + r_out_parallel; 
+}
+
  
 inline vec3 cross(const vec3& u, const vec3& v) { 
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1], 
@@ -118,6 +125,14 @@ inline vec3 cross(const vec3& u, const vec3& v) {
 inline vec3 unit_vector(const vec3& v) { 
     return v / v.length(); 
 } 
+
+inline vec3 random_in_unit_disk() { 
+    while (true) { 
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0); 
+        if (p.length_squared() < 1) 
+            return p; 
+    } 
+}
  
 
 inline vec3 random_unit_vector() { 
